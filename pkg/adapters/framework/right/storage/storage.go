eb7741702d90e1b905c176f59b3b4591bc8cc42d
Gabriel Goller <gabrielgoller123@gmail.com>
2021-12-28 23:08:24 +0100

    :bug: Tried fixing the race conditions
    Race condition issue not fixed fully.

--- pkg/adapters/framework/right/storage/storage.go
+++ pkg/adapters/framework/right/storage/storage.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"errors"
+	"sync"
 
 	// FIXME: Maybe use interface{} as type and use Services in application/api
 	"github.com/kaffarell/discoverus/pkg/application/core/instance"
@@ -10,8 +11,11 @@ import (
 
 // Adapter implements the DbPort interface
 type Adapter struct {
-	allInstances []instance.Instance
-	allServices  []service.Service
+	allInstancesMutex sync.Mutex
+	allInstances      []instance.Instance
+
+	allServicesMutex sync.Mutex
+	allServices      []service.Service
 }
 
 // NewAdapter creates a new Adapter
@@ -23,16 +27,25 @@ func NewAdapter() *Adapter {
 }
 
 func (a *Adapter) AddService(service service.Service) error {
+	a.allServicesMutex.Lock()
+	defer a.allServicesMutex.Unlock()
+
 	a.allServices = append(a.allServices, service)
 	return nil
 }
 
 func (a *Adapter) AddInstance(serviceId string, instance instance.Instance) error {
+	a.allInstancesMutex.Lock()
+	defer a.allInstancesMutex.Unlock()
+
 	a.allInstances = append(a.allInstances, instance)
 	return nil
 }
 
 func (a *Adapter) DeleteInstance(serviceId string, instanceId string) error {
+	a.allInstancesMutex.Lock()
+	defer a.allInstancesMutex.Unlock()
+
 	for i, v := range a.allInstances {
 		if v.Id == instanceId {
 			// Remove this instance
@@ -45,6 +58,9 @@ func (a *Adapter) DeleteInstance(serviceId string, instanceId string) error {
 }
 
 func (a *Adapter) DeleteService(serviceId string) error {
+	a.allServicesMutex.Lock()
+	defer a.allServicesMutex.Unlock()
+
 	for i, v := range a.allServices {
 		if v.Id == serviceId {
 			// Remove this instance
@@ -56,7 +72,10 @@ func (a *Adapter) DeleteService(serviceId string) error {
 	return errors.New("no instance to delete found")
 }
 
-func (a Adapter) GetInstancesOfService(serviceId string) ([]instance.Instance, error) {
+func (a *Adapter) GetInstancesOfService(serviceId string) ([]instance.Instance, error) {
+	a.allInstancesMutex.Lock()
+	defer a.allInstancesMutex.Unlock()
+
 	instances := make([]instance.Instance, 0)
 	for _, v := range a.allInstances {
 		if v.ServiceId == serviceId {
@@ -67,7 +86,10 @@ func (a Adapter) GetInstancesOfService(serviceId string) ([]instance.Instance, e
 
 }
 
-func (a Adapter) GetSpecificInstance(instanceId string) (instance.Instance, error) {
+func (a *Adapter) GetSpecificInstance(instanceId string) (instance.Instance, error) {
+	a.allInstancesMutex.Lock()
+	defer a.allInstancesMutex.Unlock()
+
 	for _, v := range a.allInstances {
 		if v.Id == instanceId {
 			return v, nil
@@ -76,11 +98,17 @@ func (a Adapter) GetSpecificInstance(instanceId string) (instance.Instance, erro
 	return instance.Instance{}, errors.New("no instance with id " + instanceId + " found")
 }
 
-func (a Adapter) GetAllServices() ([]service.Service, error) {
+func (a *Adapter) GetAllServices() ([]service.Service, error) {
+	a.allServicesMutex.Lock()
+	defer a.allServicesMutex.Unlock()
+
 	return a.allServices, nil
 }
 
-func (a Adapter) GetService(serviceId string) (service.Service, error) {
+func (a *Adapter) GetService(serviceId string) (service.Service, error) {
+	a.allServicesMutex.Lock()
+	defer a.allServicesMutex.Unlock()
+
 	for _, v := range a.allServices {
 		if v.Id == serviceId {
 			return v, nil
@@ -89,6 +117,9 @@ func (a Adapter) GetService(serviceId string) (service.Service, error) {
 	return service.Service{}, errors.New("no service with id " + serviceId + " found")
 }
 
-func (a Adapter) GetAllInstances() ([]instance.Instance, error) {
-	return a.allInstances, nil
+func (a *Adapter) GetAllInstances() ([]instance.Instance, error) {
+	a.allInstancesMutex.Lock()
+	defer a.allInstancesMutex.Unlock()
+	instances := a.allInstances
+	return instances, nil
 }
